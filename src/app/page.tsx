eb4a6bef7d1667e23128b2fea45e4fb47b7b1f99
HasnainAbass <hasnainabass243@gmail.com>
2024-10-27 23:32:45 +0500

    add pera

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-    <main className="max-w-2xl mx-auto h-screen bg-amber-100 text-red-800 text-3xl font-bold">
-      <h1>Hasnain Khan Rind Bloch From Rahim Yaar Khan</h1>
+    <main className="max-w-2xl mx-auto h-screen bg-amber-100">
+      <h1 className=" text-red-800 text-3xl font-bold">Sardar Hasnain Khan Rind Bloch From Rahim Yaar Khan</h1>
+      <p className="text-blue-900 text-xl font-semibold">
+        Rahim Yaar Khan, a prominent political figure and Prime Minister of Pakistan, is renowned for his role in the Rahim Yaar Khan government. He was the leader of the Pakistan Awami League (PAL) and the National Awami League (NAL), and served as the Prime Minister from 2002 to 2008.
+      </p>
     </main>
   )
 }

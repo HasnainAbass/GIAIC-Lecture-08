69d61aca456576c957d379b32530f2a0ca3e48d9
HasnainAbass <hasnainabass243@gmail.com>
2024-10-27 23:19:33 +0500

    Add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-2xl mx-auto h-screen bg-amber-100 text-red-800 text-3xl font-bold">
       <h1>Hasnain Khan Rind Bloch From Rahim Yaar Khan</h1>
     </main>
   )
